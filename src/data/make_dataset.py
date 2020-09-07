292f45b0593a2a65ad0f7a14797fd6b4cef82ae3
Bailey Srimoungchanh <srimoungchanh.bailey@ku.edu>
2020-09-07 15:29:41 -0500

    Datasets added and transformed

---
+++ src/data/make_dataset.py
@@ -0,0 +1,23 @@
+#Libraries for data processing
+import numpy as np
+import pandas as pd
+
+#Opening the data
+#----------------
+#unemp : US Unemployment data by county from 1990-2016
+#funding : US Public Food Assistance from 1969-2019
+#min_wage : US Minimum Wage by State from 1968-2017
+
+unemp = pd.read_csv('../../data/external/output.csv')
+funding = pd.read_csv('../../data/external/datasets_10128_1434247_SNAP_history_1969_2019.csv')
+min_wage = pd.read_csv('../../data/external/datasets_46796_84992_Minimum Wage Data.csv',encoding='iso-8859-1')
+
+#Grouping by year and averaging all of the county rates into one
+#central value for approximation of the entire US unemployment rate
+unemp.groupby('Year').mean().to_csv('../../data/interim/unemployment by year.csv')
+
+#Removing unnecessary data, restricting to the 1990-2016 Fiscal Years and Total Benefits
+funding[21:48].loc[:,['Fiscal Year','Total Benefits(M)']].to_csv('../../data/interim/benefits by year.csv')
+
+#Pull the CPI information, average it across the year for all states, trim year down to 1990-2016
+min_wage.groupby('Year').mean()['CPI.Average'][22:-1].to_csv('../../data/interim/cpi average by year.csv')
